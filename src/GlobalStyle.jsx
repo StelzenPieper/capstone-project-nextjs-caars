a17b3d128a1664d6dfe71a69ee6ada5755894f82
Sebastian Ottow <seb.ottow@gmail.com>
2022-05-11 10:08:40 +0200

    Add global Styles & Font

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GloablStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    background-color: white;
+    font-family: 'Poppins', sans-serif;
+}
+
+`;
