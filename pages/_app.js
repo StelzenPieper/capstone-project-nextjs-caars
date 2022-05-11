12fe92a333e15619025f4dd113cc7fdcd49161d7
Sebastian Ottow <seb.ottow@gmail.com>
2022-05-11 13:28:45 +0200

    Merge main into comp branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+	font-family: 'Poppins', sans-serif;
+  }
+`;
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
