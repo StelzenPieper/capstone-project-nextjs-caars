58845984ad2ec4d18983c5302a93678787e26189
Sebastian Ottow <seb.ottow@gmail.com>
2022-05-11 13:19:20 +0200

    Fix GlobalStyle with Nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import AddCarForm from '../src/components/AddCarForm';
-import { GloablStyle } from '../src/GlobalStyle';
-
 export default function Home() {
 	return (
 		<>
-			<GloablStyle />
 			<h1>hier entsteht ein neues CapStone</h1>
 			<p>Autor: Sebastian O.</p>
-			<AddCarForm />
 		</>
 	);
 }
