3b1e801145471d59d49adcedc8431c7c5f5342a2
Sebastian Ottow <seb.ottow@gmail.com>
2022-05-18 19:50:06 +0200

    Clean nanoId by inclusing directly into data array myVehicles

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -8,13 +8,15 @@ const useStore = create(
 			myVehicles: [],
 			fetchVehicleData: async vinValue => {
 				const url = `https://vindecodervehicle.com/api/v1/?id=caarsde&key=v9c7ah5xvc18vlztcvaj7cu7bs3e&vin=${vinValue}&getMoreData`;
-				const caarsId = nanoid();
 				try {
 					const response = await fetch(url);
 					const data = await response.json();
 					set(state => {
 						return {
-							myVehicles: [...state.myVehicles, { ...data, vinValue, caarsId }],
+							myVehicles: [
+								...state.myVehicles,
+								{ ...data, vinValue, caarsId: nanoid() },
+							],
 						};
 					});
 					console.log(data);
