13ec69bc692a3ac5981c9e5910cff1482047e76f
Sebastian Ottow <seb.ottow@gmail.com>
2022-05-20 10:31:36 +0200

    Delete extra comments

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic';
-//import CardGrid from '../src/components/CardGrid/CardGrid';
 
 export default function Home() {
 	//delete on mongoDB integration, only used for persist/localStorage (dynamic import with NO SSR form next.js docu --> https://nextjs.org/docs/advanced-features/dynamic-import)
