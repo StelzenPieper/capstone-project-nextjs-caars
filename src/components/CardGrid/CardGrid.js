092563f617842652731374a62d73bc4d389b9be4
Sebastian Ottow <seb.ottow@gmail.com>
2022-05-25 09:17:57 +0200

    Add styling and positionning to DeleteCar component

--- src/components/CardGrid/CardGrid.js
+++ src/components/CardGrid/CardGrid.js
@@ -1,13 +1,11 @@
 import StyledFlex from '../../../styles/StyledFlex';
-import StyledDiv from '../../../styles/StyledDiv';
 import Typography from '../../../styles/Typography';
-import SVGIcon from '../../assets/SVGIcon/SVGIcons';
 import useStore from '../../lib/hooks/useStore';
 import FavoriteCar from '../FavoriteCar/FavoriteCar';
+import DeleteCar from '../DeleteCar/DeleteCar';
 
 export default function CardGrid() {
 	const myVehicles = useStore(state => state.myVehicles);
-	const deleteVehicle = useStore(state => state.deleteVehicle);
 
 	return (
 		<StyledFlex margin="4vh 10px 12vh 10px" gap="40px" alignItems="center">
@@ -23,19 +21,10 @@ export default function CardGrid() {
 						gap="5px"
 					>
 						<FavoriteCar caarsId={vehicle.caarsId} />
+						<DeleteCar caarsId={vehicle.caarsId} />
 						<Typography variant="h4" textDecoration="underline">
 							{vehicle.vehicleModelSeriesName}
 						</Typography>
-						<StyledDiv height="10px" position="absolute" top="10px" right="10px">
-							<button
-								type="button"
-								onClick={() => {
-									deleteVehicle(vehicle.caarsId);
-								}}
-							>
-								<SVGIcon variant="trash" size="15px" color="red" />
-							</button>
-						</StyledDiv>
 						<div>
 							<Typography variant="default">VIN: {vehicle.vinValue}</Typography>
 							<Typography variant="default">
